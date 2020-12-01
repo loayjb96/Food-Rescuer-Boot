50728e456b55cb0e069511935728c7edc53821c2
bashbash96 <amjad.bashiti.96@gmail.com>
2020-12-01 16:35:00 +0200

    Added num of servings handler

--- handlers.py
+++ handlers.py
@@ -39,3 +39,23 @@ def handle_location_response(message, request, id_obj_map):
     res_location = request.get('message').get('location')
     location.set_address(res_location['latitude'], res_location['longitude'])
     id_obj_map[message.get_id()].set_location(location)
+    if type(id_obj_map[message.get_id()]) == Reciver:
+        handle_num_of_servings(message, request, id_obj_map)
+
+
+def handle_num_of_servings(message, request, id_obj_map):
+    servings_options = get_inline_buttons(['1', '2', '3', '4'])
+    data = {
+        "chat_id": message.get_id(),
+        "reply_markup": servings_options
+    }
+    send_post_message(data.get('chat_id'), 'How many servings the meal?', data)
+
+
+def handle_num_of_servings_response(message, request, id_obj_map):
+    print("ENTERED")
+    answer = request['callback_query']['data']
+    id = message.get_id()
+    send_get_message(id, f"Your answer is {answer}")
+
+
