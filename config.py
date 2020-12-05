88234ae0ef5e384baf1d93dd37e7572bd6c8ab99
loayjb96 <50362994+loayjb96@users.noreply.github.com>
2020-12-05 09:39:07 +0200

    config file

---
+++ config.py
@@ -0,0 +1,8 @@
+
+TOKEN=''
+URL = ''
+PATH = ''
+TELEGRAM_INIT_WEBHOOK_URL = f'https://api.telegram.org/bot{TOKEN}/setWebhook?url={URL}{PATH}'
+
+MESSAGES_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+DB_PASSWORD=''
